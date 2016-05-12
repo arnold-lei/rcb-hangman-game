54d2c47a224dd2c23685a1dfef9815c9508c5fe7
Arnold Lei <arnold.lei88@gmail.com>
2016-05-12 16:43:36 -0400

    I am starting to understand what is going on

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,19 +3,28 @@
   var nameBank =['Billie Holiday', 'Ella Firtzgerald', 'Artie Shaw','Duke Ellington', 'Benny Goodman'];
 
   var selectName = chooseName(nameBank);
+  var nameLength = selectName.length
 
   var artistName = document.getElementById('artistName');
 
   var guesses =['a', 'e', 'i', 'o', 'u'];
 
+  var results = [];
+
   var guessLeft =  9;
 
-  var result = blanks(selectName);
+  var result = blanks(results, nameLength);
 
   document.getElementById('guessLeft').innerText = guessLeft;
 
-  artistName.textContent = result;
+
 
   revealAt(guesses, selectName, result);
+
+  // insertToDoc(guesses, guessLetters);
+
+  artistName.textContent = result;
+
+document.getElementById('guessLetters').innerText = guesses;
 
 
--- assets/js/rules.js
+++ assets/js/rules.js
@@ -1,11 +1,10 @@
 // fills in the blanks
-function blanks(name) {
-  var msg = '';
-  for (i in name) {
-    msg = '_ ' + msg;
+function blanks(result, name) {
+  for(i = 0; i < name; ++i) {
+    result.push('_' + ' ');
+    }   
+    return result
   }
-  return msg;
-}
 
 function chooseName(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -15,18 +14,24 @@ function chooseName(arr) {
 
 function revealAt(guess, name, result){
   for (i = 0; i < name.length; ++i){
-    for (x = 1; x < guess.length; ++x){
+    for (x = 0; x < guess.length; ++x){
       if (guess[x] === name[i]){
-        result[x] = guess[x];
-        console.log(guess[x]);
+       result[i] = ' ' + guess[x];
       }
     }
   }
 }
 
-// function guessResults(name, guess) {
-//   for (i = 0; i < name.length; ++i){
-//     for( name.indexOf())
+function insertGuess(arr){
+  arr.push(document.getElementById("userGuess").submit());
+}
+
+// function insertToDoc (arr, id){
+//   var text = ''
+//   for (i = 0; i > arr.length; ++i){
+//     text += arr[i];
 //   }
+//   document.getElementById(id).innerText += text; 
 // }
+
 
